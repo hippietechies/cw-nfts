a5e01d36075c97fe8b1d2e2c80d9054cff184f42
Hippie <janan1990@gmail.com>
2022-06-19 18:10:22 +0800

    submsgexecution

--- contracts/cw721-fixed-price/src/contract.rs
+++ contracts/cw721-fixed-price/src/contract.rs
@@ -176,7 +176,7 @@ pub fn execute_receive(
 mod tests {
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info, MOCK_CONTRACT_ADDR};
-    use cosmwasm_std::{from_binary, to_binary, SubMsgExecutionResponse, SubMsgResult};
+    use cosmwasm_std::{from_binary, to_binary, SubMsgResponse, SubMsgResult};
     use prost::Message;
 
     // Type for replies to contract instantiate messes
@@ -242,7 +242,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: INSTANTIATE_TOKEN_REPLY_ID,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
@@ -346,7 +346,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: INSTANTIATE_TOKEN_REPLY_ID,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
@@ -412,7 +412,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: 10,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
@@ -453,7 +453,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: 1,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
@@ -496,7 +496,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: INSTANTIATE_TOKEN_REPLY_ID,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
@@ -585,7 +585,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: INSTANTIATE_TOKEN_REPLY_ID,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
@@ -638,7 +638,7 @@ mod tests {
 
         let reply_msg = Reply {
             id: INSTANTIATE_TOKEN_REPLY_ID,
-            result: SubMsgResult::Ok(SubMsgExecutionResponse {
+            result: SubMsgResult::Ok(SubMsgResponse {
                 events: vec![],
                 data: Some(encoded_instantiate_reply.into()),
             }),
